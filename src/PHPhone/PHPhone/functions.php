c5388f575f2e97fe1ce6b91636e76fe51e8dc2c0
Mike Knooihuisen <mike@simplysolutionscoding.com>
2018-11-16 16:07:09 -0500

    Working functions.php

--- src/PHPhone/PHPhone/functions.php
+++ src/PHPhone/PHPhone/functions.php
@@ -10,3 +10,8 @@ use PHPhone\PHPhone\Utils\Logger;
 function logger($level = null, $message = null) {
 	return Logger::getInstance($level, $message);
 }
+
+function env($varname, $default = false) {
+	$varname = getenv($varname);
+	return ($varname === false)? $default : $varname;
+}
